860cde8cb6b4fe9efecf4935f6091bc3dd3cd8ab
chastom <delakata@gmail.com>
2019-09-28 11:46:31 +0300

    Add project files.

---
+++ BattleX2019-MultiplayerGame/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BattleX2019_MultiplayerGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
